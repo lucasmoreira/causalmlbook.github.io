45e91fa84e444d2370d2a151e951f8a18ac3f42a
Frederik Bornemann <f.bornemann00@gmail.com>
2022-01-09 03:18:43 +0100

    add basePath and assetPrefix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
