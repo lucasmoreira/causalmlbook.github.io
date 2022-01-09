6b8cf3c523758ba4ca0fc56c02005a7faff9c132
Frederik Bornemann <f.bornemann00@gmail.com>
2022-01-09 04:03:46 +0100

    example pages for deploy testing

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
