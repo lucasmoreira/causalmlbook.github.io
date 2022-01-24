a946067c2ef1d36b7f57df29488263368e6db1aa
Frederik Bornemann <frebor@gmx.de>
2022-01-24 17:31:03 +0100

    add banner.data & favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,24 @@ class CustomDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="assets/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="assets/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="assets/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="assets/favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />
--- src/sections/banner.js
+++ src/sections/banner.js
@@ -11,37 +11,14 @@ import {
 } from "theme-ui";
 import { Link as ScrollLink } from "react-scroll";
 import src from "constants/src.data";
+import BannerContent from "constants/banner.data";
 
 export default function Banner() {
   return (
     <section sx={styles.banner} id="home">
       <Container sx={styles.banner.container}>
         <Box sx={styles.banner.contentBox}>
-          <Heading as="h1" variant="heroPrimary">
-            Causal ML Book
-          </Heading>
-          <span
-            sx={{
-              height: "4px",
-              width: ["40vw", "40vw", "40vw", "30vw", "20vw"],
-              backgroundColor: "secondary",
-              borderRadius: "2px",
-              mb: 4,
-            }}
-          ></span>
-          <Text as="p" variant="heroSecondary">
-            An introduction to the emerging fusion of modern statistical
-            (machine learning) inference and
-            <Text variant="heroSecondary" color="secondary">
-              {" "}
-              causal inference methods.
-            </Text>
-            <br />
-            <br />
-            The book introduces ideas from classical structural equation models
-            (SEMs) and their modern AI equivalent, directed acyclical graphs
-            (DAGs) and structural causal models (SCMs)
-          </Text>
+          <BannerContent />
           <Flex>
             <ScrollLink
               to="book"
